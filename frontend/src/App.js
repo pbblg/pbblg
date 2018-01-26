48fb6f6f041f7acb70be628ba16c26c278676f68
sebaks <d.kirillof@gmail.com>
2018-01-26 16:39:03 +0200

    Draft pop cardt from not played deck

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,107 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+function NotPlayedCard(props) {
+
+    const card = props.card;
+    const style = {
+        zIndex: card,
+        top: -(card * 2) + 'px',
+        left: -(card * 2) + 'px'
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="card" style={style}>
+            <div className="card-shirt"></div>
+        </div>
     );
-  }
+}
+
+function NotPlayedCardDeck(props) {
+
+    const cards = props.cards;
+    const deckCards = cards.map((card) =>
+        <NotPlayedCard key={card.toString()} card={card} />
+    );
+
+    return (
+        <div className="table-half table-left deck">
+            {deckCards}
+        </div>
+    );
+}
+
+function LastPlayedCard(props) {
+
+    const card = props.card;
+
+    return (
+        <div className="table-half table-right">
+            <div className="card ">
+                <p>{card}</p>
+            </div>
+        </div>
+    );
+}
+
+function Table(props) {
+    return (
+        <div className="table">
+            <NotPlayedCardDeck cards={props.notPlayedCards} />
+            <LastPlayedCard card={props.lastPlayedCard} />
+        </div>
+    );
+}
+
+class TestPult extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            notPlayedCards: props.notPlayedCards
+        };
+        console.log(this.state);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+
+        this.setState(function(prevState) {
+            prevState.notPlayedCards.pop();
+            console.log({
+                notPlayedCards: prevState.notPlayedCards
+            });
+            return {
+                notPlayedCards: prevState.notPlayedCards
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="table test-pult">
+                <button onClick={this.handleClick} className="test-button">Pop card from not played deck</button>
+            </div>
+        );
+    }
+}
+
+
+class App extends Component {
+
+    render() {
+
+        const notPlayedCards = [1,2,3,4,5,6];
+        const lastPlayedCard = [7];
+
+        return (
+            <div>
+                <Table notPlayedCards={notPlayedCards} lastPlayedCard={lastPlayedCard} />
+                <TestPult notPlayedCards={notPlayedCards} />
+            </div>
+        );
+    }
 }
 
 export default App;
