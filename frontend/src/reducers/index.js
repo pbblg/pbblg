0250b9d2038be7aa90a373da2de2789c3eb36628
sebaks <d.kirillof@gmail.com>
2018-02-28 20:16:11 +0200

    Fix reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -8,30 +8,24 @@ const initialState = {
 
 const app = (state = initialState, action) => {
     switch (action.type) {
+
         case NEW_GAME_WAS_CREATED:
-            return (() => {
-                let newState = Object.assign({}, state);
-                newState.games[action.game.id] = action.game;
-                return newState;
-            })();
-
         case PLAYER_JOINED_GAME:
-
-            return (() => {
-                let newState = Object.assign({}, state);
-                newState.games[action.game.id] = action.game;
-                return newState;
-            })();
+            
+            let newGames = {};
+            newGames[action.game.id] = action.game;
+            return Object.assign({}, state, {
+                games: Object.assign({}, state.games, newGames)
+            });
 
         // case 'JOIN_GAME':
         //     return state
 
         case 'RECEIVE_GAME_WELCOME_STATE':
-            return (() => {
-                let newState = Object.assign({}, state);
-                newState.games = action.data.gamesForJoin;
-                return newState;
-            })();
+
+            return Object.assign({}, state, {
+                games: Object.assign({}, state.games, action.data.gamesForJoin)
+            });
 
         case 'LOGIN_PLAYER':
             cookies.set('auth', action.playerName)
